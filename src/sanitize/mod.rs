dc5dbfa7f398e8c03418df658d26b9baf1e0f890
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-09-28 01:11:35 +0200

    feat(sanitize): Write into mod-settings.dat when given a --mods-dir

--- src/sanitize/mod.rs
+++ src/sanitize/mod.rs
@@ -1,5 +1,6 @@
 pub mod parser;
 pub mod runner;
+pub mod settings;
 
 use std::sync::{Arc, atomic::AtomicBool};
 
@@ -10,6 +11,7 @@ use crate::{
         config::{GlobalConfig, SanitizeConfig},
         utils,
     },
+    sanitize::settings::{ModSettings, ModSettingsScopeName, ModSettingsValue},
 };
 
 pub async fn run(
@@ -31,6 +33,39 @@ pub async fn run(
     )?;
     // Validate the found save files
     utils::validate_save_files(&save_files)?;
+
+    // Update belt-sanitizer mod settings
+    if let Some(ref mods_dir) = sanitize_config.mods_dir {
+        let dat_file = &mods_dir.join("mod-settings.dat");
+        let mut ms = ModSettings::load_from_file(dat_file)?;
+
+        // Prod check tick
+        ms.set(
+            ModSettingsScopeName::Startup,
+            "belt-sanitizer-production-check-tick",
+            Some(ModSettingsValue::Int(sanitize_config.ticks as i64)),
+        );
+
+        // Items
+        if let Some(ref items) = sanitize_config.items {
+            ms.set(
+                ModSettingsScopeName::Startup,
+                "belt-sanitizer-production-items",
+                Some(ModSettingsValue::String(items.clone())),
+            );
+        }
+
+        // Fluids
+        if let Some(ref fluids) = sanitize_config.fluids {
+            ms.set(
+                ModSettingsScopeName::Startup,
+                "belt-sanitizer-production-fluids",
+                Some(ModSettingsValue::String(fluids.clone())),
+            );
+        }
+
+        ms.save_to_file(dat_file)?;
+    }
 
     // Report
     let runner = runner::SanitizeRunner::new(sanitize_config.clone(), factorio);
