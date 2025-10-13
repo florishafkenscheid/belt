385293e7640340f61f0b79a17c8d14b08a00b7ac
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-10-13 02:21:20 +0200

    refactor(blueprint): Slim everything down, use what exists instead of making new functions

--- src/blueprint/mod.rs
+++ src/blueprint/mod.rs
@@ -9,15 +9,12 @@ use std::{
     sync::{Arc, atomic::AtomicBool},
 };
 
-use crate::core::{
-    FactorioExecutor, GlobalConfig, Result, config::BlueprintBenchmarkConfig,
-    output::ensure_output_dir, utils,
-};
+use crate::core::{FactorioExecutor, GlobalConfig, Result, config::BlueprintConfig, output, utils};
 
 /// Run all of the benchmarks, capture the logs and write the results to files.
 pub async fn run(
     global_config: GlobalConfig,
-    benchmark_config: BlueprintBenchmarkConfig,
+    benchmark_config: BlueprintConfig,
     running: &Arc<AtomicBool>,
 ) -> Result<()> {
     tracing::info!(
@@ -42,12 +39,12 @@ pub async fn run(
         .output
         .as_deref()
         .unwrap_or_else(|| Path::new("."));
-    ensure_output_dir(output_dir)?;
+    output::ensure_output_dir(output_dir)?;
     tracing::debug!("Output directory: {}", output_dir.display());
 
     // Run the benchmarks
-    let runner = runner::BlueprintBenchmarkRunner::new(benchmark_config.clone(), factorio);
-    let (_, _) = runner.run_all(blueprint_files, running).await?;
+    let runner = runner::BlueprintRunner::new(benchmark_config.clone(), factorio);
+    runner.run_all(blueprint_files, running).await?;
 
     Ok(())
 }
--- src/blueprint/runner.rs
+++ src/blueprint/runner.rs
@@ -4,22 +4,19 @@ use std::path::PathBuf;
 use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 
-use super::BlueprintBenchmarkConfig;
-use crate::benchmark::parser::BenchmarkResult;
-use crate::benchmark::runner::{BenchmarkRunner, VerboseData};
 use crate::core::FactorioExecutor;
 use crate::core::Result;
-use crate::core::config::BenchmarkConfig;
-use crate::core::factorio::FactorioBlueprintRunSpec;
+use crate::core::config::BlueprintConfig;
+use crate::core::factorio::FactorioRunSpec;
 
-pub struct BlueprintBenchmarkRunner {
-    config: BlueprintBenchmarkConfig,
+pub struct BlueprintRunner {
+    config: BlueprintConfig,
     factorio: FactorioExecutor,
 }
 
 /// Runs the benchmarks, keeps a progress bar updated and returns results.
-impl BlueprintBenchmarkRunner {
-    pub fn new(config: BlueprintBenchmarkConfig, factorio: FactorioExecutor) -> Self {
+impl BlueprintRunner {
+    pub fn new(config: BlueprintConfig, factorio: FactorioExecutor) -> Self {
         Self { config, factorio }
     }
 
@@ -28,43 +25,29 @@ impl BlueprintBenchmarkRunner {
         &self,
         blueprint_files: Vec<PathBuf>,
         running: &Arc<AtomicBool>,
-    ) -> Result<(Vec<BenchmarkResult>, Vec<VerboseData>)> {
-        // convert all the bps, then run with the regular runner
-        let mut converted_save_paths: Vec<PathBuf> = Vec::new();
-        for bp_file in &blueprint_files {
-            let new_save_path = self
+    ) -> Result<()> {
+        for _bp_file in &blueprint_files {
+            // inject mod settings
+            let _prefix = &self.config.prefix;
+
+            let _output = self
                 .factorio
-                .run_for_blueprint(
-                    FactorioBlueprintRunSpec {
+                .run_for_ticks(
+                    FactorioRunSpec {
                         save_file: self.config.base_save_path.as_path(),
-                        blueprint_file: bp_file.as_path(),
-                        blueprint_stable_ticks: self.config.blueprint_stable_ticks,
-                        mods_dir: self.config.mods_dir.as_path(),
-                        data_dir: self.config.data_dir.as_path(),
+                        ticks: 100, // Arbitrary, (should) happen in a few ticks
+                        mods_dir: self.config.mods_dir.as_deref(),
+                        verbose_all_metrics: false,
                         headless: self.config.headless,
                     },
                     running,
                 )
                 .await?;
-            converted_save_paths.push(new_save_path);
+
+            // check existance
+            let _data_dir = &self.config.data_dir;
         }
-        return BenchmarkRunner::new(
-            BenchmarkConfig {
-                saves_dir: self.config.blueprints_dir.clone(), // doesn't matter, not really used...
-                ticks: self.config.ticks,
-                runs: self.config.runs,
-                pattern: self.config.pattern.clone(),
-                output: self.config.output.clone(),
-                template_path: self.config.template_path.clone(),
-                mods_dir: Some(self.config.mods_dir.clone()),
-                run_order: self.config.run_order.clone(),
-                verbose_metrics: self.config.verbose_metrics.clone(),
-                strip_prefix: self.config.strip_prefix.clone(),
-                headless: self.config.headless,
-            },
-            FactorioExecutor::new(self.factorio.executable_path().to_path_buf()),
-        )
-        .run_all(converted_save_paths, running)
-        .await;
+
+        Ok(())
     }
 }
